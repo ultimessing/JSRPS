de5a3121a4dfb94ca2c256ec1ace0b1c525dcae3
ultimessing <ovrik120@gmail.com>
2024-01-15 18:22:17 +0200

    Add RPS choice function

--- RPS.js
+++ RPS.js
@@ -2,5 +2,18 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-choice = parseInt(prompt("Take your pick. Rock(1), paper(2) or scissors(3)?"))
-computerChoice = getRandomInt(2) + 1;
+function getRPSChoice(num) {
+    switch (num) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default: return "Rock";
+    }
+}
+
+promptString = ("Take your pick. Rock(1), paper(2) or scissors(3)?");
+choice = getRPSChoice(parseInt(promptString));
+computerChoice = getRPSChoice(getRandomInt(2) + 1);
